463f3acebe62f5c818082b20e6175d9ae0f0dc57
Michael Herring <mherring@CHLMichlHerring.home>
2024-09-18 11:33:56 -0500

    updated server.js

--- server.js
+++ server.js
@@ -8,18 +8,12 @@ app.get('/run-test/:testName', (req, res) => {
   const testName = req.params.testName;
   const scriptPath = `/Users/mherring/projects/playwrightmemorialcare/memorialcareplaywright/runTestAndCapture.sh ${testName}`;
 
-
-  exec(`nohup ${scriptPath} > nohup.out 2> nohup.err &`, (error, stdout, stderr) => {
+  exec(`nohup ${scriptPath} > /dev/null 2>&1 &`, (error, stdout, stderr) => {
     if (error) {
       console.error(`Error executing script: ${error.message}`);
       return res.status(500).send('Failed to trigger test.');
     }
-  
-    const fs = require('fs');
-    const output = fs.readFileSync('nohup.out', 'utf8');
-    const errorLog = fs.readFileSync('nohup.err', 'utf8');
-    
-    res.send(`Test script for ${testName} launched successfully.\nOutput Logs: ${output}\nError Logs: ${errorLog}`);
+    res.send(`Test script for ${testName} launched successfully.`);
   });
 });
 
